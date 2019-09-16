fccfd4b38bff2c931bb7025ec5ecdb395f67b46e
Alexander Baladin <my@maggit.ru>
2019-09-16 11:52:04 +0300

    Updates

---
+++ precache-manifest.b098a75291f1a82439b038f110a9a560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3913685eca3041f9023f09678990e664",
+    "url": "/creativeGen/index.html"
+  },
+  {
+    "revision": "dca485b193c13edb360a",
+    "url": "/creativeGen/static/css/main.da6028e5.chunk.css"
+  },
+  {
+    "revision": "3052f104cf28536c785f",
+    "url": "/creativeGen/static/js/2.3696ee1f.chunk.js"
+  },
+  {
+    "revision": "dca485b193c13edb360a",
+    "url": "/creativeGen/static/js/main.3d2b92a8.chunk.js"
+  },
+  {
+    "revision": "548710310c594301eafe",
+    "url": "/creativeGen/static/js/runtime~main.37b18be3.js"
+  }
+]);
